52682d3821d19e444aa1366bfe3b6e2ea6910443
raz avraham <razavraham3@gmail.com>
2019-02-10 12:20:30 +0200

    section2 lec 14

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,7 +15,16 @@ namespace Vidly.Controllers
 
             var movie = new Movie() { Name = "Shrek!" };
 
+
+
             return View(movie);
         }
+
+
+        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        //public ActionResult ByReleaseYear(int year,int month)
+        //{
+        //    return Content(year +"/" + month);
+        //}
     }
 }
