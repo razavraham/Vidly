2b757668614f00599d1b7960579ed5c14653e8ca
raz avraham <razavraham3@gmail.com>
2019-02-12 11:18:01 +0200

    after migrations add initial-Identity

---
+++ Vidly/Models/IdentityModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+namespace Vidly.Models
+{
+    public class IdentityModels
+    {
+
+        public class ApplicationUser : IdentityUser
+        {
+            public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+            {
+                // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+                var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+                // Add custom user claims here
+                return userIdentity;
+            }
+        }
+
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+        {
+
+            public ApplicationDbContext()
+                : base("DefaultConnection")
+            {
+
+            }
+
+            public static ApplicationDbContext Create()
+            {
+                return new ApplicationDbContext();
+            }
+        }
+
+
+    }
+}
